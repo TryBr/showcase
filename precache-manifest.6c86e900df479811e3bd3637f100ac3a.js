6a70191750ae0f793bb83549f393ad72d9c77a59
Дмитрий Бородин <trybr@MacBook-Pro-Dmitrij.local>
2020-07-13 07:11:11 +0200

    Updates

---
+++ precache-manifest.6c86e900df479811e3bd3637f100ac3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c0bcb2d284891a5b4eae674bef18e2",
+    "url": "/showcase/index.html"
+  },
+  {
+    "revision": "23ac9ba0ab994dc49df5",
+    "url": "/showcase/static/css/main.30063de4.chunk.css"
+  },
+  {
+    "revision": "cbf056ff850c670e4aa8",
+    "url": "/showcase/static/js/2.57c75a82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/showcase/static/js/2.57c75a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ac9ba0ab994dc49df5",
+    "url": "/showcase/static/js/main.2624f01b.chunk.js"
+  },
+  {
+    "revision": "520d3acac7b62759aab7",
+    "url": "/showcase/static/js/runtime-main.108a18e6.js"
+  },
+  {
+    "revision": "f97014e89e348fdd55118130bf1280c1",
+    "url": "/showcase/static/media/showcase-item-img.f97014e8.jpeg"
+  }
+]);
